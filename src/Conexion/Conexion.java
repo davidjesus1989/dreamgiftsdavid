4200b1c64896980b83f264f8e7830eb8cc349f99
Usuario <Usuario@DESKTOP-NJBPVFC>
2021-08-04 11:29:09 -0400

    Trabajando en actualizacion de datos en crud canal

---
+++ src/Conexion/Conexion.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexion;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+/**
+ *
+ * @author Usuario
+ */
+
+public class Conexion {
+    
+      private Connection con;
+    
+    
+    public  Connection ConexionBD(){
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost/dreamgifts","root","");
+            
+        }catch(Exception e){
+            System.out.println("no conetado: "+e);
+     }
+        return con;
+    }//fin metodo
+
+    public Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
+
+
+
+        
---
+++ src/negocio/Canal.java
@@ -0,0 +1,53 @@
+
+package negocio;
+
+/**
+ *
+ * @author rafay
+ */
+public class Canal {
+    private int CAN_ID_CANAL;
+    private String CAN_NOMBRE;
+    private boolean CAN_ESTADO;
+
+    public Canal() {
+        this.CAN_ID_CANAL = 0;
+        this.CAN_NOMBRE = "";
+        this.CAN_ESTADO = true;
+    }
+
+    public Canal(int CAN_ID_CANAL, String CAN_NOMBRE, boolean CAN_ESTADO) {
+        this.CAN_ID_CANAL = CAN_ID_CANAL;
+        this.CAN_NOMBRE = CAN_NOMBRE;
+        this.CAN_ESTADO = CAN_ESTADO;
+    }
+
+    public int getCAN_ID_CANAL() {
+        return CAN_ID_CANAL;
+    }
+
+    public void setCAN_ID_CANAL(int CAN_ID_CANAL) {
+        this.CAN_ID_CANAL = CAN_ID_CANAL;
+    }
+
+    public String getCAN_NOMBRE() {
+        return CAN_NOMBRE;
+    }
+
+    public void setCAN_NOMBRE(String CAN_NOMBRE) {
+        this.CAN_NOMBRE = CAN_NOMBRE;
+    }
+
+    public boolean isCAN_ESTADO() {
+        return CAN_ESTADO;
+    }
+
+    public void setCAN_ESTADO(boolean CAN_ESTADO) {
+        this.CAN_ESTADO = CAN_ESTADO;
+    }
+    
+    
+    
+    
+}
+
